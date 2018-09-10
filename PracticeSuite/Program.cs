71f0aad26e5102d21a9b7a42c4fa0a6cd6c29900
HarveySembhy <h.sembhy@gmail.com>
2018-09-10 13:23:21 +0100

    Added a new project - Looking to use this as a Automation Practice Suite from now on.

--- PracticeSuite/Program.cs
+++ PracticeSuite/Program.cs
@@ -10,6 +10,38 @@ namespace PracticeSuite
     {
         static void Main(string[] args)
         {
+            // list of automation suites
+
+            /*Login form
+            
+            
+            CAN A USER ENTER IN A CORRECT USERNAME AND WRONG PASSWORD
+            If a User enters a correct username
+            AND
+            Enters an incorrect password
+            WHEN user submits their details
+            THEN an error message will appear: "You have entered an incorrect password"
+
+            CAN A USER ENTER IN AN INCORRECT USERNAME AND CORRECT PASSWORD
+            IF a user enters in an incorrect username
+            AND
+            Enters a correct password
+            WHEN user submits their details
+            THEN an error message will appear: "You have entered an incorrect username"
+
+            CAN A USER SUCCESSFUL LOGIN AND LOGOUT
+            IF a user enters in their details correctly
+            WHEN user logs in they will see the login page
+            WHEN user clicks the logout button
+            THEN the user will be brought back to the login page.
+
+
+
+
+
+            */
+
+
         }
     }
 }
